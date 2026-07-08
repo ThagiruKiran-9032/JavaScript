cd85616ac8f5546617a4e70f78e84a0b1f03be93
ThagiruKiran-9032 <thagirukiran9032@gmail.com>
2026-07-08 17:57:52 +0530

    Operations in Java Script

---
+++ operations.js
@@ -0,0 +1,41 @@
+
+// Arithmetic Operations
+let x= 5;
+let y = 10;
+console.log("addition of x and y is: ", x + y);
+console.log("subtraction of x and y is: ", y - x);
+console.log("multiplication :",x*y)
+console.log("modulus :",y%x)
+console.log("division :",y/x)
+console.log("exponent :",y**x)
+
+// Logical Operations
+
+console.log("AND :", x && y)
+console.log("OR :", x || y)
+console.log("NOT :", !x)
+
+
+// Comparison Operations
+console.log("Equal :", x == y)
+console.log("Strict Equal :", x === 5)
+console.log("Not Equal :", x != y)
+console.log("Greater Than :", x > y)
+console.log("Less Than :", x < y)
+console.log("Greater Than or Equal :", x >= 5)
+console.log("Less Than or Equal :", y <= 10)
+
+// Ternary Operations
+let result = x > y ? "x is greater" : "y is greater"
+console.log("Ternary Result :", result)
+
+
+// Bitwise Operations
+
+console.log("BITWISE AND :",x&y)
+console.log("BITWISE OR :",x|y)
+console.log("BITWISE XOR :",x^y)
+console.log("LEFT SHIFT :",x<<y)
+console.log("RIGHT SHIFT :",x>>y)
+
+
